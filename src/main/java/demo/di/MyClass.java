a977df5b2d5c1ba4892859029ea19e9afbe90e48
somkiat <somkiat.p@gmail.com>
2016-08-13 00:54:48 +0700

    3 dependency injection

--- src/main/java/demo/di/MyClass.java
+++ src/main/java/demo/di/MyClass.java
@@ -1,31 +1,24 @@
 package demo.di;
 
-class Dependency1 {}
-class Dependency2 {}
+import javax.inject.Inject;
 
-final class DependencyFactory {
-    private static Dependency1 dependency1;
-    private static Dependency2 dependency2;
-
-    public static Dependency1 provideDependency1() {
-        if(dependency1 == null) {
-            dependency1 = new Dependency1();
-        }
-        return dependency1;
-    }
-
-    public static Dependency2 provideDependency2() {
-        if(dependency2 == null) {
-            dependency2 = new Dependency2();
-        }
-        return dependency2;
-    }
+class Dependency1 {
+    public void doSomething() {}
 }
 
+
 public class MyClass {
+
+    @Inject
     private Dependency1 dependency1;
 
+    @Inject
     public MyClass(Dependency1 dependency1) {
         this.dependency1 = dependency1;
+    }
+
+    @Inject
+    public void callMethod(Dependency1 dependency1) {
+        dependency1.doSomething();
     }
 }
